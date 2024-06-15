b00a0b26880a818dcc169025d106a253387574e6
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2024-06-15 15:33:35 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -3,8 +3,10 @@ const commands = {
   '/report': 'Reporting Tool: codetestgroup.github.io/chats/report',
   '/ping': 'Want to play ping pong? | PING | PONG | PING | PONG | PING | PONG',
   '/guidelines': 'Guidelines/Rules: 1. No Swearing | 2. Be Kind | 3. Be respectfull | Want to report something? Use /report',
+  '/timetable': 'Commands: /welcome /report / ',
   '/game': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
   '/homework': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
   '/help': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
   '/timetable': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
+  '/commands': 'Available commands: /welcome /report /ping /guidelines /game /homework /help /timetable',
 };
