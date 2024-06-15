5667b99d56ad8d2b88338d8873ea60dca2b1386a
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2024-06-15 16:06:42 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -9,6 +9,18 @@ const commands = {
   '/help': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
   '/timetable': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
   '/commands': 'Available commands: /welcome /report /ping /guidelines /game /homework /help /timetable',
-  '/commands test': 'Test',
+
+  '/timetable': 'Please put the day and week number [a/b]. Example: /timetable-monday-a',
+  '/timetable-monday-a': 'Timetable Monday Week: A Lessons: 1. Ilearn 2. English 3. PE 4. Sciecne 5. Geography 6. French',
+  '/timetable-monday-b': 'Timetable Monday Week: B Lessons: 1. Ilearn 2. History 3. Sciece 4. English 5. French 6. PE',
+  '/timetable-tuesday-a': 'Timetable Tuesday Week: A Lessons: Lessons: 1. Ilearn 2. ART 3. English 4. French 5. EP 6.Math ',
+  '/timetable-tuesday-b': 'Timetable Tuesday Week: B Lessons: 1. Ilearn 2. Histoy 3. Math 4. ICT 5. English 6. French',
+  '/timetable-wednesday-a': 'Timetable Wednesday Week: A Lessons: 1. Ilearn 2. ICT 3. DT 4. Math 5. Science 6. DT',
+  '/timetable-wednesday-b': 'Timetable Wednesday Week: B Lessons: 1. Ilearn 2. English 3. Math 4. Sceicne 5. Drama 6. History',
+  '/timetable-thursday-a': 'Timetable Thursday Week: A Lessons: 1. Ilearn 2. English 3. History 4.  French 5. Science 6. DT',
+  '/timetable-thursday-b': 'Timetable Thursday Week: B Lessons: 1. Ilearn 2. PE 3. Geography 4. Drama 5. Music 6. Math',
+  '/timetable-friday-a': 'Timetable Friday Week: A Lessons: 1. Ilearn 2. Math 3. EP 4. Geography 5. PE 6. Geography',
+  '/timetable-friday-b': 'Timetable Friday Week: B Lessons: 1. Ilearn 2. Math 3. Art 4. Music 5. English 6. Science',
+
   
 };
