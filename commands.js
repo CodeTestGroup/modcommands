bc01bb27fed597ef6cb5ea06b3f634858c16343a
CodeTestGroup (CTG) <147273420+CodeTestGroup@users.noreply.github.com>
2024-06-15 15:12:21 +0100

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,10 @@
+const commands = {
+  '/welcome': 'Welcome to the 7BE1 group!',
+  '/report': 'Reporting Tool: codetestgroup.github.io/chats/report',
+  '/ping': 'Want to play ping pong? | PING | PONG | PING | PONG | PING | PONG',
+  '/guidelines': 'Guidelines/Rules: 1. No Swearing | 2. Be Kind | 3. Be respectfull | Want to report something? Use /report',
+  '/game': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
+  '/homework': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
+  '/help': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
+  '/timetable': 'This command will be coming soon! Please do /commands for the list of avaliable commands!',
+};
